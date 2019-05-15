7b31fb330444a65a322ae900f9f4c1389bfbacad
MarBry111 <mrmareczek111@gmail.com>
2019-05-15 22:11:57 +0200

    solved

--- 08/08.py
+++ 08/08.py
@@ -1,6 +1,19 @@
-import numpy as np 
+import numpy as np
 from numba import jit
 
-d_a = np.genfromtxt('08.txt', delimiter = 1) #digits array
-d_a = d_a.reshape(1,d_a.shape[0]*d_a.shape[1])
-print(d_a.shape)
+d_a = np.genfromtxt('08.txt', delimiter=1)  # digits array
+d_a = d_a.reshape(d_a.shape[0] * d_a.shape[1])
+
+
+def substrings(n, x):
+    return np.fromfunction(lambda i, j: x[i + j], (len(x) - n + 1, n), dtype=int)
+
+
+a = substrings(13, d_a)
+
+a_multiply = np.prod(a, axis=1)
+
+ind = np.where(a_multiply == np.max(a_multiply))
+
+print(a[ind])
+print(np.max(a_multiply))
